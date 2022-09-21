b978a658260c75f7b3174597188455752f4d6507
RubaCodes <rubacodes@gmail.com>
2022-09-21 14:36:28 +0100

    codice fiscale base

--- Program.cs
+++ Program.cs
@@ -5,11 +5,15 @@ Clienti secondoCliente = new Clienti("Giovanni", "Verdi");
 Console.WriteLine(banca.Nome);
 //aggiunta due clienti
 banca.AddCliente(primoCliente);
+primoCliente.Stipendio = 30000;
 banca.AddCliente(secondoCliente);
+
+//test modifica cliente
+banca.EditCliente(secondoCliente, "Gianluca", "Gianfranchi", 22000);
 
 
 //test ricerca cliente : nome e cognome
-List<Clienti> risultatoRicerca = banca.SearchCliente("Giovanni", "Verdi");
+List<Clienti> risultatoRicerca = banca.SearchCliente("Gianluca", "Gianfranchi");
 if (risultatoRicerca.Count == 0)
 {
     Console.WriteLine("Spiacente, non risulta nessun cliente con quel nome e cognome nel database");
